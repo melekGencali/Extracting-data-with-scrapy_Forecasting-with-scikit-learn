9d4b4611f637cbef0f962cb58e47c623e0a766d4
melekGencali <68080787+melekGencali@users.noreply.github.com>
2020-07-10 20:30:41 +0300

    Add files via upload

--- scrapyProje/items.py
+++ scrapyProje/items.py
@@ -1,1 +1,25 @@
+# -*- coding: utf-8 -*-
 
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ScrapyprojeItem(scrapy.Item):
+    # define the fields for your item here like:
+
+
+    baslik = scrapy.Field()
+    kategori = scrapy.Field()
+    fiyat = scrapy.Field()
+    features = scrapy.Field()
+    yildiz = scrapy.Field()
+    yorum = scrapy.Field()
+    yorumYildizi = scrapy.Field()
+    yonetmen = scrapy.Field()
+    oyuncular = scrapy.Field()
+
+    pass
